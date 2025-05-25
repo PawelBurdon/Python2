a0bcc3d22f2a94f2aca8733542438726bafb72d1
Paweł Burdon <pawelburdon@pawes-macbook-air.home>
2025-05-25 23:27:42 +0200

    hangman

---
+++ hangman/main.py
@@ -0,0 +1,44 @@
+import random as rand
+
+word_list = ["flowers", "onion", "ring"]
+
+
+chosen_word = word_list[rand.randint(0, len(word_list)-1)]
+print(chosen_word)
+
+
+placeholder = ""
+for word in chosen_word:
+    placeholder += "_"
+print(placeholder)
+
+lives = 6
+
+correct_letters = []
+
+
+while(True):
+    print("Lives left:", lives)
+    guess = input("Guess a letter: ").lower()
+
+    display = ""
+
+    for letter in chosen_word:
+        if letter == guess:
+            display += guess
+            correct_letters.append(guess)
+        elif letter in correct_letters:
+            display += letter
+        else:
+            display += "_"
+    print(display)
+
+    if guess not in chosen_word:
+        lives -= 1
+        if lives == 0:
+            print("You lose!")
+            break
+
+    if "_" not in display:
+        print("You win!")
+        break
